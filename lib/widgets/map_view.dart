593abb897055fec82cdeaef8e6932baf90a1e56c
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2025-02-12 00:26:54 +0100

    straggler

--- lib/widgets/map_view.dart
+++ lib/widgets/map_view.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-// Import for Android features.
-// import 'package:webview_flutter_android/webview_flutter_android.dart';
-// // Import for iOS features.
-// import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-
 class MapView extends StatefulWidget {
   final String mapUrl;
 
@@ -22,38 +17,18 @@ class _MapViewState extends State<MapView> {
   @override
   void initState() {
     super.initState();
-    // #docregion platform_features
-    // late final PlatformWebViewControllerCreationParams params;
-    // if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-    //   params = WebKitWebViewControllerCreationParams(
-    //     allowsInlineMediaPlayback: true,
-    //     mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-    //   );
-    // } else {
-    //   params = const PlatformWebViewControllerCreationParams();
-    // }
-
-    // final WebViewController controller =
-    //     WebViewController.fromPlatformCreationParams(params);
-    // #enddocregion platform_features
-
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse(widget.mapUrl));
-    // WebViewPlatform.instance =
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Map View")),
+      appBar: AppBar(title: Text("Map View"), automaticallyImplyLeading: false),
       body: Stack(children: [
         WebViewWidget(
           controller: _controller,
-          // gestureRecognizers: TapGestureRecognizer()
-          //   ..onTap = () {
-          //     _openMap(widget.mapUrl);
-          //   }
         ),
         InkWell(onTap: () => _openMap(widget.mapUrl)),
       ]),
@@ -62,7 +37,7 @@ class _MapViewState extends State<MapView> {
 
   Future<void> _openMap(String uri) async {
     final Uri url = Uri.parse(uri);
-    if (!await launchUrl(url /*, mode: LaunchMode.externalApplication*/)) {
+    if (!await launchUrl(url)) {
       throw 'Could not launch $url';
     }
   }
